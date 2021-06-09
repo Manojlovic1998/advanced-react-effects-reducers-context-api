07cfd0a0688a22aba5f5d2a1b22db07627098de5
Manojlovic1998 <manojlovic911@hotmail.com>
2021-06-09 15:42:00 +0200

    Use useEffect hook for debouncing user input logic

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -12,9 +12,18 @@ const Login = (props) => {
     const [formIsValid, setFormIsValid] = useState(false);
 
     useEffect(() => {
-        setFormIsValid(
-            enteredEmail.includes('@') && enteredPassword.trim().trim().length > 6
-        );
+        const identifier = setTimeout(() => {
+            console.log("Checking for validity!");
+            setFormIsValid(
+                enteredEmail.includes('@') && enteredPassword.trim().trim().length > 6
+            );
+        }, 500);
+
+        // clean up function
+        return () => {
+            console.log("Cleanup");
+            clearTimeout(identifier);
+        };
     }, [enteredEmail, enteredPassword]);
 
     const emailChangeHandler = (event) => {
